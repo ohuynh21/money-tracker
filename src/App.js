93a8e48d9ff689d5ad959ac2f5455c8c728e9db1
ohuynh21 <100678197+ohuynh21@users.noreply.github.com>
2024-04-01 21:20:00 -0700

    set up page design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>100<span>.00</span></h1>
+      <form>
+        <div className='basic'>
+          <input type='text' placeholder='Purchased Item'></input>
+          <input type='datetime-local'></input>
+        </div>
+        <div>
+          <input type='text' placeholder='Add a Note'></input>
+        </div>
+        <button>Add New Transaction</button>
+      </form>
+      <div className='transactions'>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>Name</div>
+            <div className='description'>Description</div>
+            </div>
+          <div className='right'>
+            <div className='pos-price'>$100</div>
+            <div className='date'>10/9/2001</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>Name</div>
+            <div className='description'>Description</div>
+            </div>
+          <div className='right'>
+            <div className='neg-price'>$100</div>
+            <div className='date'>10/9/2001</div>
+          </div>
+        </div>
+        <div className='transaction'>
+          <div className='left'>
+            <div className='name'>Name</div>
+            <div className='description'>Description</div>
+            </div>
+          <div className='right'>
+            <div className='price'>$100</div>
+            <div className='date'>10/9/2001</div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
